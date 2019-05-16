1740c84cae35dd54776558f5c059ce0029eb5ec1
Bright Lu <brightlu@udel.edu>
2019-05-16 10:22:34 -0400

    added functions to output test

--- outputtest.c
+++ outputtest.c
@@ -4,32 +4,51 @@
 #include <string.h>
 #include <stdlib.h>
 
-void display_dot(pi_framebuffer_t *dev) {
-  uint16_t white = getColor(255, 255, 255);
-  dev->bitmap->pixel[1][1]=white;
+void display_dot(pi_framebuffer_t *dev, int x, int y) {
+  uint16_t red = getColor(255, 0, 0);
+  dev->bitmap->pixel[x][y]=red;
 }
 
-void display_twodot(pi_framebuffer_t *dev) {
+void display_two_dot(pi_framebuffer_t *dev, int x, int y) {
+  uint16_t green = getColor(0, 255, 0);
+  dev->bitmap->pixel[y][x]=green;
+  dev->bitmap->pixel[y][x+1]=green;
+}
+
+void display_three_dot(pi_framebuffer_t *dev, int x, int y) {
   uint16_t blue = getColor(0, 0, 255);
-  dev->bitmap->pixel[1][3]=blue;
-  dev->bitmap->pixel[1][4]=blue;
+  dev->bitmap->pixel[y][x]=blue;
+  dev->bitmap->pixel[y][x-1]=blue;
+  dev->bitmap->pixel[y][x+1]=blue;
+}
+
+void display_z_piece(pi_framebuffer_t *dev, int x, int y) {
+  uint16_t yellow = getColor(51, 255, 255);
+  dev->bitmap->pixel[x][y]=yellow;
+  dev->bitmap->pixel[x-1][y]=yellow;
+  dev->bitmap->pixel[x][y-1]=yellow;
+  dev->bitmap->pixel[x+1][y-1]=yellow;
+}
+
+void display_r_piece(pi_framebuffer_t *dev, int x, int y) {
+  uint16_t orange = getColor(255, 128, 0);
+  dev->bitmap->pixel[y][x]=orange;
+  dev->bitmap->pixel[y][x-1]=orange;
+  dev->bitmap->pixel[y+1][x-1]=orange;
+  dev->bitmap->pixel[y-1][x]=orange;
 }
 
 int main(void) {
   pi_framebuffer_t *dev = getFBDevice();
-  display_dot(dev);
-  display_twodot(dev);
-  //clearBitmap(dev->bitmap,0);  
-}
-#include "framebuffer.h"
-#include "sense.h"
-#include <stdio.h>
-
-void display_dot(pi_framebuffer_t *dev) {
-  uint16_t white = getColor(255, 255, 255);
-  dev->bitmap->pixel[i][j]=white;
-}
-
-int main(void) {
-  display_dot(pi_framebuffer_t *dev);
+  char input;
+  while (!input) {
+    display_dot(dev, 1, 1);
+    display_two_dot(dev, 3, 1);
+    display_three_dot(dev, 3, 2);
+    display_z_piece(dev, 6, 6);
+    display_r_piece(dev, 6, 1);
+    scanf("%c", &input);
+  }
+  clearBitmap(dev->bitmap, 0);
+  return 0;
 }
